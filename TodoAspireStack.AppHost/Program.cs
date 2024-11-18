7f6afd888347a2f28c5bd059e9607956536494ce
Jakob spiel <jako5457@gmail.com>
2024-11-18 13:09:33 +0100

    added pgadmin example

--- TodoAspireStack.AppHost/Program.cs
+++ TodoAspireStack.AppHost/Program.cs
@@ -4,6 +4,14 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 // Database
 var SqlServer = builder.AddPostgres("Postgress");
+
+
+if (builder.Environment.IsDevelopment())
+{
+    //Web interface on port 8080
+    //SqlServer.WithPgAdmin(container => container.WithHostPort(8080));
+}
+
 var todoDB = SqlServer.AddDatabase("TodoDB");
 
 //TodoApp
