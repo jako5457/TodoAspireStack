0e0db1c247834b868c855c185e51727a64ad1f3d
Jakob spiel <jako5457@gmail.com>
2024-11-19 08:18:36 +0100

    Added custom container

--- TodoAspireStack.AppHost/Program.cs
+++ TodoAspireStack.AppHost/Program.cs
@@ -4,18 +4,26 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 // Database
 var SqlServer = builder.AddPostgres("Postgress");
-
-
-if (builder.Environment.IsDevelopment())
-{
-    //Web interface on port 8080
-    //SqlServer.WithPgAdmin(container => container.WithHostPort(8080));
-}
-
 var todoDB = SqlServer.AddDatabase("TodoDB");
 
 //TodoApp
 var TodoApi = builder.AddProject<Projects.Todo_Api>("todo-api")
                      .WithReference(todoDB);
+
+#region PgAdmin
+if (builder.Environment.IsDevelopment())
+{
+    
+    //Web interface on port 8080
+    //SqlServer.WithPgAdmin(container => container.WithHostPort(8080));
+
+}
+#endregion PgAdmin
+
+#region Custom container
+// gets jako5457/shops:latest from docker hub
+//var ShopApp = builder.AddContainer("ShopIndex","jako5457/shops","latest")
+//                     .WithEndpoint(port: 8081, targetPort: 80,"http");
+#endregion
 
 builder.Build().Run();
